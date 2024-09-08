ee6964c7bc937f0fd98408aceafb3da4c03952b2
suin <suinyeze@gmail.com>
2024-09-09 07:37:28 +0900

    chore: add kcl and kcl-language-server to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-2405.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    kclpkgs.url = "github:appthrust/kcl-nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-2405, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-2405, flake-utils, kclpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-				pkgs-2405 = import nixpkgs-2405 {
-					inherit system;
-				};
+        pkgs-2405 = import nixpkgs-2405 {
+          inherit system;
+        };
+        kcl = kclpkgs.default.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-          	fish
-          	gnugrep
-          	gnused
-          	gum
-          	helmfile
-          	just
+            fish
+            gnugrep
+            gnused
+            gum
+            helmfile
+            just
             crossplane-cli
             k3d
             kubectl
@@ -33,10 +35,12 @@
             yamllint
             yq-go
             zsh
-          ] ++ (with pkgs-2405; []);
+            kcl.cli
+            kcl.language-server
+          ] ++ (with pkgs-2405; [ ]);
           shellHook = ''
-						export KUBECONFIG="$(pwd)/kubeconfig.yaml"
-            exec fish
+            						export KUBECONFIG="$(pwd)/kubeconfig.yaml"
+                        exec fish
           '';
         };
         formatter = pkgs.nixpkgs-fmt;
